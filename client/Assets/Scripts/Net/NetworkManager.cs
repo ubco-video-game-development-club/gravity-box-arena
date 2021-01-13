9e60598c54bf26f088fe2a85d34251244afccfcd
Kurtis Knodel <kurtisknodel@gmail.com>
2021-01-13 11:18:00 -0800

    Started implemented high level network API

--- client/Assets/Scripts/Net/NetworkManager.cs
+++ client/Assets/Scripts/Net/NetworkManager.cs
@@ -1,30 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class NetworkManager : MonoBehaviour
 {
+	public static NetworkManager Singleton { get { return singleton; } }
+	private static NetworkManager singleton = null;
+
 	public bool IsConnected { get { return client.Connected; } }
 	public UnityEvent OnConnected { get { return onConnected; } }
 	public UnityEvent OnDisconnect { get { return onDisconnect; } }
 	public UnityEvent OnJoinedLobby { get { return onJoinedLobby; } }
 
 	[SerializeField] private string hostname;
+	[SerializeField] private float networkDelta;
 	[SerializeField] private UnityEvent onConnected;
 	[SerializeField] private UnityEvent onDisconnect;
 	[SerializeField] private UnityEvent onJoinedLobby;
     private LowLevelClient client;
 	private string authKey;
 	private int lobbyId;
+	private List<NetworkObject> trackedObjects;
 
 	void Awake()
 	{
+		if(singleton != null)
+		{
+			Destroy(this);
+			return;
+		}
+
+		singleton = this;
 		client = new LowLevelClient(hostname);
 		lobbyId = -1;
+		trackedObjects = new List<NetworkObject>();
+
+		StartCoroutine(NetworkUpdate());
 	}
-	
+
 	public void Connect()
 	{
 		client.Connect();
@@ -46,6 +62,42 @@ public class NetworkManager : MonoBehaviour
 		}
 
 		StartCoroutine(JoinOrCreateLobbyRoutine());
+	}
+
+	public void Track(NetworkObject obj) => trackedObjects.Add(obj);
+	public void Untrack(NetworkObject obj) => trackedObjects.Remove(obj);
+
+	private IEnumerator NetworkUpdate()
+	{
+		WaitForSecondsRealtime waitForSeconds = new WaitForSecondsRealtime(networkDelta);
+
+		while(IsConnected)
+		{
+			using(MemoryStream mStream = new MemoryStream())
+			{
+				using(BinaryWriter writer = new BinaryWriter(mStream))
+				{
+					foreach(NetworkObject obj in trackedObjects)
+					{
+						writer.Write(obj.Id);
+						obj.SendData(writer);
+					}
+				}
+
+				byte[] buffer = mStream.ToArray();
+				client.SyncData(buffer);
+			}
+
+			using(MemoryStream mStream)
+			{
+				while(client.TryGetData(out string from, out byte[] data))
+				{
+					mStream.Write(data, 0, data.Length);
+				}
+
+				//TODO: Send data to networked objects
+			}
+		}
 	}
 
 	private IEnumerator JoinOrCreateLobbyRoutine()
